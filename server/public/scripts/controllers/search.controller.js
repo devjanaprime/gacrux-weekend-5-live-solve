2a99902ad328062215b997616933fc26f874c24d
devjanaprime <dev@primeacademy.io>
2018-03-05 11:29:26 -0600

    ready for step 4

--- server/public/scripts/controllers/search.controller.js
+++ server/public/scripts/controllers/search.controller.js
@@ -11,10 +11,11 @@ app.controller('SearchController', ['$http', 'FavoritesService', function ($http
     self.searchApi = function( searchDetails ) {
         console.log( 'in SearchController searchApi:', searchDetails );
         const urlBase = 'https://www.swapi.co/api';
-        let searchUrl = urlBase + '/people/?search=qui';
+        let searchUrl = urlBase + '/' + searchDetails.resource + '/?search=' + searchDetails.query;
+        console.log( 'search url:', searchUrl );
         $http.get( searchUrl ).then( function ( response ) {
             console.log( 'back from swapi with:', response );
-            // self.results = response.data.results;
+            self.results = response.data.results;
         })
         .catch( function( response ) {
             console.log( 'error from swapi:', response);
